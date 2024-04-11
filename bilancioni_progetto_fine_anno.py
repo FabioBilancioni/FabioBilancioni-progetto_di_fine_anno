1b47647938880fac62c0190fa06a58ed4ef2b96f
FabioBilancioni <147493642+FabioBilancioni@users.noreply.github.com>
2024-04-11 13:13:29 +0200

    Add files via upload

---
+++ bilancioni_progetto_fine_anno.py
@@ -0,0 +1,22 @@
+import random
+
+def play_game():
+    choices = ['sasso', 'carta', 'forbici']
+    computer = random.choice(choices)
+    utente = input("Scegli tra sasso, carta e forbici: ")
+
+    print(f"Il computer ha scelto: {computer}")
+    print(f"Tu hai scelto: {utente}")
+
+    if utente == computer:
+        print("Pareggio!")
+    elif utente == 'sasso' and computer == 'forbici' or \
+         utente == 'carta' and computer == 'sasso' or \
+         utente == 'forbici' and computer == 'carta':
+        print("Hai vinto!")
+    elif utente == 'sasso' and computer == 'carta' or\
+         utente == 'carta' and computer == 'forbici' or\
+         utente == 'forbici' and computer == 'forbici':
+        print("Hai perso!")
+
+play_game()
